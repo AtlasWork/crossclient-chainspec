bea1752d84ae93023c6778ab56b1802c93488bd2
Patrick-Joseph: Lismore <patricklismore@gmail.com>
2018-02-15 22:41:39 +0000

    updated main net

--- startParity.sh
+++ startParity.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-parity --chain AtlasParity.json --port 57200 --jsonrpc-port 8539 --nat extip:YOURIP --reserved-peers atlastestnetbootnodes.txt
+parity --chain AtlasParityMainNet.json --port 57200 --jsonrpc-port 8539 --nat extip:YOURIP --reserved-peers atlasmainnetbootnodes.txt
